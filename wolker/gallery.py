28916ede34d2b846dcfa506700bd98a37a3182fd
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-02-09 13:14:52 +0100

    debug

--- wolker/gallery.py
+++ wolker/gallery.py
@@ -6,7 +6,7 @@ import os
 import cgi
 
 def append(files, filename):
-    files.append(common.return_file(filename)
+    files.append(common.return_file(filename))
     
 def main(typ=''):
     files = []
@@ -19,14 +19,14 @@ def main(typ=''):
     postfiles = os.listdir('genouts')
     postfiles.sort(reverse=True)
     for filename in postfiles:
-        append(files, f'genouts/{filename}'
+        append(files, f'genouts/{filename}')
         if typ == 'admin':
             key, _ = filename.split('.')
             files.append(common.replace_and_return_file(
                     'gallery_admin_sharebutton.html', {'KEY': key}))
         
             append(files, 'gallery_sep.html')
-    append(files. 'footer.html')
+    append(files, 'footer.html')
     
     return files
 
