f177d790ca9f9f9918bb7ed2ce7ec962b8a90a2f
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-02-09 02:32:46 +0100

    galerie: oddělení funkce a vzhledu

--- wolker/gallery.py
+++ wolker/gallery.py
@@ -11,13 +11,8 @@ form = cgi.FieldStorage()
 typ = form.getvalue("typ", "")
 
 if typ == 'admin':
-    print('''<div id="fb-root"></div>
-    <script async defer crossorigin="anonymous"
-    src="https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v19.0&appId=184130128287110"
-    nonce="hJb6HzSN"></script>''')
-
-print('<a href="index.py">Zpátky na začátek</a>')
-print('<br>')
+    common.write_out_file(f'gallery_admin_head.html')
+common.write_out_file(f'gallery_head.html')
 
 files = os.listdir('genouts')
 files.sort(reverse=True)
@@ -25,8 +20,8 @@ for filename in files:
     common.write_out_file(f'genouts/{filename}')
     if typ == 'admin':
         key, _ = filename.split('.')
-        print('<br>')
-        print(f'<div class="fb-share-button" data-href="https://ufal.mff.cuni.cz/AIvK/edupo/wolker/post.py?key={key}" data-layout="" data-size=""><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fufal.mff.cuni.cz%2FAIvK%2Fedupo%2Fwolker%2Fpost.py%3Fkey%3D{key}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Sdílet</a></div>')
-    print('<br><hr><br>')
+        common.replace_and_write_out_file(
+                'gallery_admin_sharebutton.html', {'KEY': key})
+    common.write_out_file(f'gallery_sep.html')
     
 common.footer()
