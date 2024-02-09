81d4b7bca693e61ea636a196306b563d5523e4db
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-02-09 11:32:26 +0100

    static

--- wolker/app.py
+++ wolker/app.py
@@ -6,5 +6,13 @@ import common
 def page():
     return common.page()
 
+
+# serve static files in the given subdirectories
+@bottle.route('/<directory:path:re:(css|genimgs|fa-symbols|fonts).*>/<style>')
+def static(directory, style):
+    return bottle.static_file(style, directory)
+
+
+
 application = bottle.default_app()
 
