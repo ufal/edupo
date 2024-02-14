e95636d2fdf014844ec4534b874c0bac07b77e2a
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-02-14 19:37:24 +0100

    multithreaded so that it does not keep hanging

--- wolker/wolker_interactive.py
+++ wolker/wolker_interactive.py
@@ -6,6 +6,7 @@ with open('apikey.txt') as infile:
 
 from openai import OpenAI
 import time
+import multiprocessing
 
 import logging
 logging.basicConfig(
@@ -42,14 +43,17 @@ def talk(message="Napište báseň o přírodě ve městě."):
     result, _, _ = talk_threaded(message)
     return result[-1]
 
-def talk_threaded(message="Napište báseň o přírodě ve městě.",
+def _talk_threaded(d,
+        message="Napište báseň o přírodě ve městě.",
         assistant_id=ASSISTANT_ID,
         thread_id=None):
+
+    starttime = time.time()
+    
     if not thread_id:
         thread = client.beta.threads.create()
         thread_id = thread.id
-
-    starttime = time.time()
+    d['thread_id'] = thread_id
 
     # add message to thread
     msg = client.beta.threads.messages.create(
@@ -74,9 +78,26 @@ def talk_threaded(message="Napište báseň o přírodě ve městě.",
         run = client.beta.threads.runs.retrieve(
             thread_id=thread_id,
             run_id=run.id
-        )        
+        )
 
-    result, roles = get_thread_messages(thread_id)
+
+def talk_threaded(message="Napište báseň o přírodě ve městě.",
+        assistant_id=ASSISTANT_ID,
+        thread_id=None):
+
+    manager = multiprocessing.Manager()
+    d = manager.dict()
+
+    p = multiprocessing.Process(target=_talk_threaded,
+            args=(d, message, assistant_id, thread_id))
+    p.start()
+    p.join(MAXTIME)
+    if p.is_alive():
+        p.terminate()
+        raise Exception('Max time reached')
+    else:
+        thread_id = d['thread_id']
+        result, roles = get_thread_messages(thread_id)
 
     return result, roles, thread_id
 
