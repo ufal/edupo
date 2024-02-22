0bc08b598494356829f63f7297d6f0c50cb02a65
David Mareček <marecek@ufal.mff.cuni.cz>
2024-02-22 12:06:04 +0100

    prvni pokus o generování toho, co se má shodovat v rýmu

--- rhyming/analyze.py
+++ rhyming/analyze.py
@@ -1,23 +1,46 @@
+import sys
 from rhymetagger import RhymeTagger
 
-poem = [
-	"Tell me not, in mournful numbers,",
-	"Life is but an empty dream!",
-	"For the soul is dead that slumbers,",
-	"And things are not what they seem.",
-	"Life is real! Life is earnest!",
-	"And the grave is not its goal;",
-	"Dust thou art, to dust returnest,",
-	"Was not spoken of the soul.",
-	"Not enjoyment, and not sorrow,",
-	"Is our destined end or way;",
-	"But to act, that each tomorrow",
-	"Find us farther than today.",
-]
+# read poem
+poem = []
+with open(sys.argv[1]) as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            poem.append(line)
+f.close()
 
 rt = RhymeTagger()
-rt.load_model(model='en')
 
-rhymes = rt.tag(poem, output_format=3) 
-print(rhymes)
+# Slots to hold current stanza and poem id
+rt.stanza_id = 0
+rt.poem_id = 0
+
+# Container for dataset. Items correspond to a single line
+# and hold a tuple (rhyme_word, poem_id, stanza_id)
+rt.data = list()
+        
+# Container for vocabulary. Each key is a rhyme_word found in
+# the dataset and holds a tuple ([components], final_ngram)
+rt.rhyme_vocab = dict()     
+
+# Perform tagging        
+rt.verbose = False        
+
+# Parameters
+rt.transcribed = False        
+rt.lang = "cs"
+rt.vowel_length = False
+rt.stress = True
+rt.syll_max = 2
+rt.ngram_length = 10
+
+rt.add_to_model(poem)
+
+if len(poem) != len(rt.data):
+    sys.stderr.write("Error: Line counts don't match!\n")
+else:
+    for i, line in enumerate(rt.data):
+        components = rt.rhyme_vocab[line[0]][0]
+        print(poem[i], list(reversed(components)))
 
