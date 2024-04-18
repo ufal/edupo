158118e3fdc0714741dff0f2ccf761ebfa3015e6
David Mareček <marecek@ufal.mff.cuni.cz>
2024-04-18 10:57:58 +0200

    přidán pravidlový nástroj na detekci ne-dvojhlásek

--- kveta/morph.py
+++ kveta/morph.py
@@ -40,8 +40,36 @@ class Morphodita:
             # Iterate over tokens
             for i in range(len(self.lemmas)):
                 lemma = self.lemmas[i]
-                token = self.tokens[i]                                                
-                
+                token = self.tokens[i]
+
+                str_lemma = re.sub('\-.*$|\:.*$| *\^.*$| *;.*$|\,.*$', '', lemma.lemma)
+                str_token = text[token.start : token.start + token.length]
+
+
+                # TODO: mark non-diphtong candidates
+                nodip = ""
+                nodip_prefixes = ["anglo", "bystro", "celo", "dlouho", "dobro", "do", "eko", "kraso", "krátko", "indo", "jedno", "jiho", "lehko", "makro", "malo", "mikro", "mnoho", "mravo", "nízko", "novo", "polo", "pro", "po", "prvo", "pseudo", "rychlo", "samo", "severo", "sladko", "staro", "sto", "středo", "těžko", "velko", "věro", "vnitro", "východo", "vysoko", "západo", "zlato", "zlo"]
+                nodip_nolemma_suffixes = ["uchý", "učka", "učkově", "učkový", "učně", "učný", "učovat", "uk", "uka", "ukově", "ukový"]
+                for prefix in nodip_prefixes:
+                    if str_lemma.startswith(prefix+'u'):
+                        suffix = str_lemma[len(prefix):]
+                        if len(suffix) == 0:
+                            continue
+                        # tag the suffix
+                        forms2 = Forms()
+                        lemmas2 = TaggedLemmas()
+                        tokens2 = TokenRanges()
+                        tokenizer2 = self.tagger.newTokenizer()
+                        tokenizer2.setText(suffix)
+                        tokenizer2.nextSentence(forms2,tokens2)
+                        self.tagger.tag(forms2,lemmas2,0) # The last argument is 0 since we don't want to use morphological guesser.
+                        # if suffix was not recognized by the tagger (tag X) or suffix is in the list
+                        if lemmas2[0].tag[0] != 'X' or suffix in nodip_nolemma_suffixes:
+                            p = str_token.find(prefix) + len(prefix) 
+                            nodip = str_token[:p] + '₇' + str_token[p:] # ₇ is used here as a sign in places where ou is not a diphtong
+                            #print("SUCCESS", nodip)
+                        break
+
                 # If token is a punctuation mark store it as attribute of 
                 # previous word (if any)
                 if lemma.tag.startswith('Z'):
@@ -50,12 +78,14 @@ class Morphodita:
 
                 # ...otherwise append token tags to current line
                 else:
-                    poem[l]['words'].append({
-                        'token': text[token.start : token.start + token.length],
-                        'lemma': re.sub('\-.*$|\:.*$| *\^.*$| *;.*$|\,.*$', '', lemma.lemma),
-                        'morph': lemma.tag,
-                        'sentence': s,
-                    })
+                    features = {'token': str_token,
+                                'lemma': str_lemma,
+                                'morph': lemma.tag,
+                                'sentence': s
+                                }
+                    if nodip:
+                        features['nodip'] = nodip
+                    poem[l]['words'].append(features)
                     
                 # If token followed by <newline>: increase the line index
                 if (len(text) >= token.start + token.length + 1
