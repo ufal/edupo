a7daefb4491cef173acb62d57e0dff88a9620a21
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-04-22 15:22:39 +0200

    logs in separate dir

--- webapp/app.sh
+++ webapp/app.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 f=$(date "+%Y-%m-%d-%H-%M-%S")
-l=app.$f.log
-e=app.$f.err
+l=logs/app.$f.log
+e=logs/app.$f.err
 
 T=10s
 
+echo Starting server loop, logs in $l and $e, autorestart in $T
+
 while true;
 do
-    echo Starting server, logs in $l and $e
+    date
+    echo Starting server...
     authbind --deep flask run --host=0.0.0.0 --port 80
     echo Server stopped, waiting $T for restart...
     sleep $T
+    echo
 done > $l 2> $e
 
 # requires sudo:
