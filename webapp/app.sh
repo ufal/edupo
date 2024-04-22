3470bc426bac94ecc52bd48b5bf56b5201d1b2e2
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-04-22 12:32:59 +0000

    bash code to run on virtual machine

--- webapp/app.sh
+++ webapp/app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-flask run --host=0.0.0.0 --port 80
+# flask run --host=0.0.0.0 --port 80
+authbind --deep flask run --host=0.0.0.0 --port 80
 
 # debug run:
 # flask run --host=0.0.0.0 --debug
