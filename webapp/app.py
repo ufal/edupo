f5609bc5cd906cfcc50ecc9aec446b9e900d32f3
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-04-29 15:37:33 +0200

    lze definovat rhyme scheme přes gen?rhyme_scheme=ABBA například

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
-from flask import Flask
+from flask import Flask, request
 import os
 from gen import generuj
 from show_poem_html import show_file 
 
 app = Flask(__name__)
 print(__name__)
+
+# TODO tohle se asi nemá dělat
+# NOTE takhle pokud POSTuju tak se nedostanu k parametrum zadanym v URL; tj.
+# nechceme dělat POST z formuláře kde by cílová URL obsahovala i GET parametry
+# (to asi stejně nechceme)
+def get_post_arg(key, default=None):
+    if request.method == 'POST':
+        return request.form.get(key, default)
+    elif request.method == 'GET':
+        return request.args.get(key, default)
+    else:
+        # TODO probably should not happen
+        return default
 
 @app.route("/")
 def hello_world():
@@ -17,8 +30,10 @@ def hello_world():
 def prdel_world():
     return "<p>Hello, Prdel!</p>"
 
-@app.route("/gen")
-def call_generuj(poet_start = 'AABB'):
+@app.route("/gen", methods=['GET', 'POST'])
+def call_generuj():
+    rhyme_scheme = get_post_arg('rhyme_scheme', 'AABB')
+    poet_start = rhyme_scheme
     verses = generuj(poet_start)
     return '<br>'.join(verses)
 
