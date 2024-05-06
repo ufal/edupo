38ddb7e0670593c7756c08034f9b87f91c43d5d6
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-05-06 16:35:24 +0200

    zobrazit básně z db

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 #coding: utf-8
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, g
 import os
 from gen import generuj
-from show_poem_html import show_file 
+import show_poem_html
+import sqlite3
+import json
+from collections import defaultdict
 
 app = Flask(__name__)
 print(__name__)
+
+DBFILE='/net/projects/EduPo/data/new.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DBFILE)
+        db.row_factory = sqlite3.Row
+    return db
+
+# close db connection at end of handling request
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.commit()  # in case commit forgotten
+        db.close()
 
 # TODO tohle se asi nemá dělat
 # NOTE takhle pokud POSTuju tak se nedostanu k parametrum zadanym v URL; tj.
@@ -44,10 +64,21 @@ def call_generuj():
     app.logger.info(f"Generated poem {clean_verses}")
     return '<br>'.join(verses)
 
-@app.route("/show")
-def call_show(filename = '78468.json'):
-    html = show_file(filename)
-    return html
+@app.route("/show", methods=['GET', 'POST'])
+def call_show():
+    poemid = get_post_arg('poemid', '78468.json', True)
+    if poemid.endswith('.json'):
+        return show_poem_html.show_file(poemid)
+    else:
+        table = get_post_arg('table', 'poems', True)
+        with get_db() as db:
+            sql = f'SELECT * FROM {table} WHERE id={poemid}'
+            result = db.execute(sql).fetchone()
+        assert result != None 
+        data = defaultdict(str, result)
+        data['body'] = json.loads(result['body'])
+        html = show_poem_html.show(data)
+        return html
 
 @app.route("/tajnejkill")
 def kill():
--- webapp/show_poem_html.py
+++ webapp/show_poem_html.py
@@ -12,16 +12,46 @@ logging.basicConfig(
 import json
 import string
 from string import Template
+from collections import defaultdict
 
 # TODO rewrite using Jinja probably?
 # from flask import render_template
 
-# Reads in file
+# rhyme is 1-based
+# nonrhyming = None, converts to 0
+RYM = ' ' + 10*string.ascii_uppercase
 
-def show_file(filename = '78468.json'):
+METRUM = {
+    'J': "jamb",
+    'T': "trochej",
+    'D': "daktyl",
+        }
 
-    with open(filename) as infile:
-        j = json.load(infile)
+def get_metrum(metre):
+    if metre in METRUM:
+        return METRUM[metre]
+    else:
+        return f"metrum {metre}"
+
+def get_metre(verse):
+    try:
+        return list(verse["metre"][0].keys())[0]
+    except:
+        logging.warning(f"Missing metre in data.")
+        return '?'
+
+def get_rhyme(verse):
+    try:
+        rhyme = 0 if verse["rhyme"] == None else verse["rhyme"]
+        int(rhyme)
+        return rhyme
+    except:
+        logging.warning(f"Missing valid rhyme in data.")
+        return 0
+
+
+def show(data):
+    data = defaultdict(str, data)
 
     with open('html/show_poem_html.html') as infile:
         html = Template(infile.read())
@@ -30,70 +60,9 @@ def show_file(filename = '78468.json'):
     with open('html/verse.html') as infile:
         verse_html = Template(infile.read())
 
-    def get_j0_key(j, key1, default=''):
-        try:
-            return j[0][key1]
-        except:
-            logging.warning(f"Missing {key1} in data.")
-            return default
-
-    def get_j0_key_key(j, key1, key2, default=''):
-        try:
-            return j[0][key1][key2]
-        except:
-            logging.warning(f"Missing {key1} {key2} in data.")
-            return default
-
-    def get_metre(verse):
-        try:
-            return list(verse["metre"][0].keys())[0]
-        except:
-            logging.warning(f"Missing metre in data.")
-            return '?'
-
-    def get_rhyme(verse):
-        try:
-            rhyme = 0 if verse["rhyme"] == None else verse["rhyme"]
-            int(rhyme)
-            return rhyme
-        except:
-            logging.warning(f"Missing valid rhyme in data.")
-            return 0
-
-
-    replacements = {}
-
-    replacements['p_title'] = get_j0_key_key(j, "biblio", "p_title")
-    replacements['p_author'] = get_j0_key_key(j, "p_author", "name")
-    replacements['schools'] = ', '.join(get_j0_key(j, "p_schools", []))
-    replacements['born'] = get_j0_key_key(j, "p_author", "born")
-    replacements['died'] = get_j0_key_key(j, "p_author", "died")
-    replacements['b_title'] = get_j0_key_key(j, "biblio", "b_title")
-    replacements['year'] = get_j0_key_key(j, "biblio", "year")
-    replacements['filename'] = filename
-    replacements[''] = ''
-    replacements[''] = ''
-
-    # print("SLOKY:", len(j[0]["body"]))
-
-    # rhyme is 1-based
-    # nonrhyming = None, converts to 0
-    RYM = ' ' + 10*string.ascii_uppercase
-
-    METRUM = {
-        'J': "jamb",
-        'T': "trochej",
-        'D': "daktyl",
-            }
-
-    def get_metrum(metre):
-        if metre in METRUM:
-            return METRUM[metre]
-        else:
-            return f"metrum {metre}"
-
     stanzas_html = []
-    for stanza in j[0]["body"]:
+    
+    for stanza in data['body']:
         verses_html = []
         for verse in stanza:
             rhyme = get_rhyme(verse)
@@ -111,10 +80,45 @@ def show_file(filename = '78468.json'):
         # TODO možná restartovat číslování rýmu po každé sloce
         # (ale někdy jde rýmování napříč slokama)
 
-    replacements['body'] = '\n'.join(stanzas_html)
+    data['stanzas'] = '\n'.join(stanzas_html)
     
-    return html.substitute(replacements)
+    return html.substitute(data)
 
+
+# Reads in file
+def show_file(filename = '78468.json'):
+
+    with open(filename) as infile:
+        j = json.load(infile)
+
+    def get_j0_key(j, key1, default=''):
+        try:
+            return j[0][key1]
+        except:
+            logging.warning(f"Missing {key1} in data.")
+            return default
+
+    def get_j0_key_key(j, key1, key2, default=''):
+        try:
+            return j[0][key1][key2]
+        except:
+            logging.warning(f"Missing {key1} {key2} in data.")
+            return default
+
+
+    data = {}
+
+    data['title'] = get_j0_key_key(j, "biblio", "p_title")
+    data['author'] = get_j0_key_key(j, "p_author", "name")
+    data['schools'] = ', '.join(get_j0_key(j, "p_schools", []))
+    data['born'] = get_j0_key_key(j, "p_author", "born")
+    data['died'] = get_j0_key_key(j, "p_author", "died")
+    data['b_title'] = get_j0_key_key(j, "biblio", "b_title")
+    data['year'] = get_j0_key_key(j, "biblio", "year")
+    data['id'] = filename
+    data['body'] = j[0]["body"]
+
+    return show(data)
 
 if __name__=="__main__":
     filename = sys.argv[1]
