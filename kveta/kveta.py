1b66d50dea4440638ad80ee9e47303c34a4f466f
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-05-13 16:59:21 +0200

    květa importovatelná

--- kveta/kveta.py
+++ kveta/kveta.py
@@ -51,88 +51,88 @@ class Kveta:
         self.html_, self.pie_data_ = html.stringify(self.poem_, self.overall_probs_, mscore)
 
 
+if __name__=="__main__":
+    filename = sys.argv[1]
 
-filename = sys.argv[1]
+    # read poem
+    text = ""
+    with open(filename) as f:
+        for line in f:
+            text += line
+    f.close()
 
-# read poem
-text = ""
-with open(filename) as f:
-    for line in f:
-        text += line
-f.close()
+    # Get parameters
 
-# Get parameters
-
-k = Kveta(text)
-k.preprocessing()
-k.pos_tagging()
-k.phonetics()
-k.syllables()
-k.line2vec()
-k.meter()
-#k.rhyme(rwindow, rpsounds, rpngrams)
-k.rhyme(6, 0.95, 0.95)
-#k.htmlprint(mscore)
-k.htmlprint()
-
-
-#output = {
-#    'pie_data': k.pie_data_,
-#    'object': {
-#        'metres': k.overall_probs_,
-#        'poem': k.poem_,
-#    },
-#}
-
-# Change the rhyming format to CCV style
-rc = dict()
-cur_num = 0
-for i, v in enumerate(k.poem_):
-    minimum = i
-    for x in v["rhyme"]:
-        if x < minimum:
-            minimum = x
-    if not minimum in rc:
-        cur_num += 1
-        rc[minimum] = cur_num
-    k.poem_[i]["rhyme"] = rc[minimum]
-
-# Put metres from k.pie_data_ into verses
-for i, v in enumerate(k.poem_):
-    letter = "T"
-    if k.pie_data_[i]["metre"] == "iamb":
-        letter = "J"
-    elif k.pie_data_[i]["metre"] == "dactyl":
-        letter = "D"
-    k.poem_[i]["metre"] = [ {letter: k.pie_data_[i]} ]
+    k = Kveta(text)
+    k.preprocessing()
+    k.pos_tagging()
+    k.phonetics()
+    k.syllables()
+    k.line2vec()
+    k.meter()
+    #k.rhyme(rwindow, rpsounds, rpngrams)
+    k.rhyme(6, 0.95, 0.95)
+    #k.htmlprint(mscore)
+    k.htmlprint()
 
 
-# Change the JSON structure to resemble CCV
-output = [
-    { 
-        "metres": k.overall_probs_,
-        "body": [ k.poem_ ]
-    }
-]
+    #output = {
+    #    'pie_data': k.pie_data_,
+    #    'object': {
+    #        'metres': k.overall_probs_,
+    #        'poem': k.poem_,
+    #    },
+    #}
 
-# Copy the streses and S/W/V into indivisual syllables
-for i, v in enumerate(k.poem_):
-    swv = v["metre"][0]["T"]["pattern"]
-    stress = v["sections"]
-    pointer = 0
-    for j, w in enumerate(v["words"]):
-        for l, s in enumerate(w["syllables"]):
-            k.poem_[i]["words"][j]["syllables"][l]["position"] = swv[pointer]
-            k.poem_[i]["words"][j]["syllables"][l]["stress"] = stress[pointer]
-            pointer += 1
+    # Change the rhyming format to CCV style
+    rc = dict()
+    cur_num = 0
+    for i, v in enumerate(k.poem_):
+        minimum = i
+        for x in v["rhyme"]:
+            if x < minimum:
+                minimum = x
+        if not minimum in rc:
+            cur_num += 1
+            rc[minimum] = cur_num
+        k.poem_[i]["rhyme"] = rc[minimum]
 
-if filename[-4:] == ".txt":
-    filename = filename[:-4]
+    # Put metres from k.pie_data_ into verses
+    for i, v in enumerate(k.poem_):
+        letter = "T"
+        if k.pie_data_[i]["metre"] == "iamb":
+            letter = "J"
+        elif k.pie_data_[i]["metre"] == "dactyl":
+            letter = "D"
+        k.poem_[i]["metre"] = [ {letter: k.pie_data_[i]} ]
 
-with open(filename+'.json', 'w', encoding='utf-8') as f:
-    json.dump(output, f, ensure_ascii=False, indent=4)
-f.close()
 
-with open(filename+'.html', 'w', encoding='utf-8') as h:
-    h.write(k.html_)
-h.close()
+    # Change the JSON structure to resemble CCV
+    output = [
+        { 
+            "metres": k.overall_probs_,
+            "body": [ k.poem_ ]
+        }
+    ]
+
+    # Copy the streses and S/W/V into indivisual syllables
+    for i, v in enumerate(k.poem_):
+        swv = v["metre"][0]["T"]["pattern"]
+        stress = v["sections"]
+        pointer = 0
+        for j, w in enumerate(v["words"]):
+            for l, s in enumerate(w["syllables"]):
+                k.poem_[i]["words"][j]["syllables"][l]["position"] = swv[pointer]
+                k.poem_[i]["words"][j]["syllables"][l]["stress"] = stress[pointer]
+                pointer += 1
+
+    if filename[-4:] == ".txt":
+        filename = filename[:-4]
+
+    with open(filename+'.json', 'w', encoding='utf-8') as f:
+        json.dump(output, f, ensure_ascii=False, indent=4)
+    f.close()
+
+    with open(filename+'.html', 'w', encoding='utf-8') as h:
+        h.write(k.html_)
+    h.close()
