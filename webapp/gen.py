51a08f5b2008c66fb1d67a223143c89facaae503
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-06-07 20:59:29 +0200

    more

--- webapp/gen.py
+++ webapp/gen.py
@@ -16,7 +16,7 @@ import random
 tokenizer = AutoTokenizer.from_pretrained("jinymusim/gpt-czech-poet")
 model = AutoModelForCausalLM.from_pretrained("jinymusim/gpt-czech-poet")
 
-def generuj(rhyme_scheme='AABB', metre='J', firstline='', year=1900):
+def generuj(rhyme_scheme='AABB', metre='J', firstword='', firstline='', year=1900):
 
     if not metre:
         metre = random.choice(['J', 'T', 'D'])
@@ -25,7 +25,11 @@ def generuj(rhyme_scheme='AABB', metre='J', firstline='', year=1900):
     if firstline:
         # TODO format:
         # D # 11 # eště # po letech v polích jsem ohlížel se ještě,
-        poet_start += firstline
+        poet_start += firstline + '\n'
+    elif firstword:
+        # TODO format:
+        # D # 11 # eště # po letech v polích jsem ohlížel se ještě,
+        poet_start += firstword
 
     # tokenize input
     tokenized_poet_start = tokenizer.encode(poet_start, return_tensors='pt')
