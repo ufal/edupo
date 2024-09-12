2a5b7ed15026a709827a95c5a8ecf61b86a61ae1
David Mareček <marecek@ufal.mff.cuni.cz>
2024-09-12 13:21:40 +0200

    Tokeny rozdelene UDpipe se merguji

--- kveta/morph.py
+++ kveta/morph.py
@@ -26,7 +26,7 @@ class Morphodita:
         text = '\n'.join([x['text'] for x in poem]) + '\n'
 
         # perform the UDPipe request to parse the whole poem
-        response = requests.get("https://lindat.mff.cuni.cz/services/udpipe/api/process?tokenizer=ranges&tagger&parser&data=" + text)
+        response = requests.get("https://lindat.mff.cuni.cz/services/udpipe/api/process?tokenizer&tagger&parser&data=" + text)
 
         l = 0 # line index
         t = 0 # token index
@@ -35,11 +35,10 @@ class Morphodita:
         split_token = False
         split_token_end = 0
         split_token_form = ""
-        split_token_range = ""
+        split_token_spaces = ""
         split_token_tags = []
         
         for line in response.json()["result"].split("\n"):
-            print(line)
             sent_id = re.match("^# sent_id = (\d+)", line)
             if sent_id:
                 s = sent_id.groups()[0]
@@ -48,7 +47,7 @@ class Morphodita:
                 if '-' in items[0]: # if the token is split into more nodes (e.g. aby -> aby + by)
                     split_token = True
                     split_token_form = items[1]
-                    split_token_range = items[9]
+                    split_token_spaces = items[9]
                     split_token_end = items[0].split("-")[-1]
                     split_token_tags = []
                     continue
@@ -62,7 +61,7 @@ class Morphodita:
                 else:
                     if split_token and items[0] == split_token_end:
                         items[1] = split_token_form
-                        items[9] = split_token_range
+                        items[9] = split_token_spaces
                         items[4] = "|".join(split_token_tags) + "|" + items[4]
                         split_token = False
 
@@ -81,14 +80,8 @@ class Morphodita:
                     
                     poem[l]['words'].append(features)
                 
-                    
-                # If the token is followed by a newline (e.g. TokenRange=45:49 and there is a newline on the position 49), increase the line index
-                #print(items[9])
-                #if text[int(items[9].split(":")[-1])] == '\n':
-                #    l += 1
-
                 # If the token is followed by a newline, increase the line index
-                if 'SpacesAfter=\n' in items[9]:
+                if 'SpacesAfter=\\n' in items[9]:
                     l += 1
             
         return poem
