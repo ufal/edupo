52b310e6475a56831d4ede7cb1519d6f25dd2a2a
David Mareček <marecek@ufal.mff.cuni.cz>
2024-09-12 11:28:05 +0200

    Morphodita nahrazena UDPipe, pridani parent a deprel

--- kveta/morph.py
+++ kveta/morph.py
@@ -1,7 +1,8 @@
-from ufal.morphodita import *
+#from ufal.morphodita import *
 import re
 import os
 import sys
+import requests
 
 sys.path.append(os.path.abspath('../scripts/diphthongs'))
 from hyphenator import Hyphenator
@@ -10,19 +11,8 @@ class Morphodita:
     
     
     def __init__(self):
-        '''
-        Initialize MorphoDiTa
-        '''
-        
-        filepath = os.path.join(os.path.dirname(__file__),
-                'dicts', 'czech-morfflex-pdt-161115.tagger')
-        self.tagger = Tagger.load(filepath)
-        self.forms = Forms()
-        self.lemmas = TaggedLemmas()
-        self.tokens = TokenRanges()
-        self.tokenizer = self.tagger.newTokenizer()
 
-        # initialize Hyphenator for detecting diphtongs
+        # initialize Hyphenator for detecting diphthongs
         with open("../scripts/diphthongs/diphthongs.patterns", 'r') as f:
             patterns = f.read().splitlines()
         self.hyp = Hyphenator(patterns)
@@ -33,92 +23,48 @@ class Morphodita:
         '''
 
         # Merge lines of poem with <newline>
-        text = '\n'.join([x['text'] for x in poem])
+        text = '\n'.join([x['text'] for x in poem]) + '\n'
 
-        # Pass poem to tokenizer
-        self.tokenizer.setText(text)
+        # perform the UDPipe request to parse the whole poem
+        response = requests.get("https://lindat.mff.cuni.cz/services/udpipe/api/process?tokenizer=ranges&tagger&parser&data=" + text)
 
-        # Line index
-        l = 0        
-        # Token index
-        t = 0
-        # Sentence index
-        s = 0
-
-        # Iterate over sentences
-        while self.tokenizer.nextSentence(self.forms, self.tokens):
-            self.tagger.tag(self.forms, self.lemmas)
-
-            # Iterate over tokens
-            for i in range(len(self.lemmas)):
-                lemma = self.lemmas[i]
-                token = self.tokens[i]
-
-                str_lemma = re.sub(r'\-.*$|\:.*$| *\^.*$| *;.*$|\,.*$', '', lemma.lemma)
-                str_token = text[token.start : token.start + token.length]
-
-
-                # mark non-diphtong candidates using rules
-                #nodip = ""
-                #nodip_prefixes = ["anglo", "bystro", "celo", "dlouho", "dobro", "do", "eko", "kraso", "krátko", "indo", "jedno", "jiho", "lehko", "makro", "malo", "mikro", "mnoho", "mravo", "nízko", "novo", "polo", "pro", "po", "prvo", "pseudo", "rychlo", "samo", "severo", "sladko", "staro", "sto", "středo", "těžko", "velko", "věro", "vnitro", "východo", "vysoko", "západo", "zlato", "zlo"]
-                #nodip_nolemma_suffixes = ["uchý", "učka", "učkově", "učkový", "učně", "učný", "učovat", "uk", "uka", "ukově", "ukový"]
-                #excluded_lemmas = ["proud", "proudit", "proudově", "proudový", "proudící"]
-                #for prefix in nodip_prefixes:
-                #    if str_lemma.startswith(prefix+'u') and not str_lemma in excluded_lemmas:
-                #        suffix = str_lemma[len(prefix):]
-                #        if len(suffix) <= 1:
-                #            continue
-                #        # tag the suffix
-                #        forms2 = Forms()
-                #        lemmas2 = TaggedLemmas()
-                #        tokens2 = TokenRanges()
-                #        tokenizer2 = self.tagger.newTokenizer()
-                #        tokenizer2.setText(suffix)
-                #        tokenizer2.nextSentence(forms2,tokens2)
-                #        self.tagger.tag(forms2,lemmas2,0) # The last argument is 0 since we don't want to use morphological guesser.
-                #        # if suffix was not recognized by the tagger (tag X) or suffix is in the list
-                #        if lemmas2[0].tag[0] != 'X' or suffix in nodip_nolemma_suffixes:
-                #            #print(suffix, lemmas2[0].tag)
-                #            p = str_token.find(prefix) + len(prefix)
-                #            nodip = str_token[:p] + '₇' + str_token[p:] # ₇ is used here as a sign in places where ou is not a diphtong
-                #            #print("SUCCESS", nodip)
-                #        break
-
-                # mark non-diphtongs candidates using Tomáš's Hyphenator
-                nodip = ""
-                segments = self.hyp.hyphenate_word(str_token.lower())
-                if len(segments) > 1:
-                    nodip = '₇'.join(segments)
-
-
-                # If token is a punctuation mark store it as attribute of 
-                # previous word (if any)
-                if lemma.tag.startswith('Z'):
+        l = 0 # line index
+        t = 0 # token index
+        s = 0 # sentence index
+        
+        for line in response.json()["result"].split("\n"):
+            sent_id = re.match("^# sent_id = (\d+)", line)
+            if sent_id:
+                s = sent_id.groups()[0]
+            if line and line[0].isdigit():
+                items = line.split("\t")
+                
+                # If token is a punctuation mark (tag starts with Z) store it as attribute of previous word (if any)
+                if items[4].startswith('Z'):
                     if len(poem[l]['words']) > 0:
-                        poem[l]['words'][-1]['punct'] = text[token.start : token.start + token.length]
-
+                        poem[l]['words'][-1]['punct'] = items[1]
 
                 # ...otherwise append token tags to current line
                 else:
-                    features = {'token': str_token,
-                                'lemma': str_lemma,
-                                'morph': lemma.tag,
+                    features = {'token': items[1],
+                                'lemma': items[2],
+                                'morph': items[4],
+                                'tok_id': items[0],
+                                'parent': items[6],
+                                'deprel': items[7],
                                 'sentence': s
                                 }
-                    if nodip:
-                        features['nodip'] = nodip
-                    poem[l]['words'].append(features)
+                    # mark non-diphtongs candidates using Tomáš's Hyphenator
+                    segments = self.hyp.hyphenate_word(items[1].lower())
+                    if len(segments) > 1:
+                        features['nodip'] = '₇'.join(segments)
                     
-                # If token followed by <newline>: increase the line index
-                if (len(text) >= token.start + token.length + 1
-                and text[token.start + token.length] == '\n'):
+                    poem[l]['words'].append(features)
+                
+                    
+                # If the token is followed by a newline (e.g. TokenRange=45:49 and there is a newline on the position 49), increase the line index
+                if text[int(items[9].split(":")[-1])] == '\n':
                     l += 1
-
-                # Increase token index
-                t = token.start + token.length
-
-            # Increase sentence index
-            s += 1
             
         return poem
 
