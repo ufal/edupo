b32ecd285d0feca2374575fff3c84d4a7af84558
David Mareček <marecek@ufal.mff.cuni.cz>
2024-11-14 15:18:30 +0100

    do celeho KCV se prida CFT a slabiky

---
+++ kveta/run_add_syllables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+INPUT_DIR=/net/projects/EduPo/data/KCV_komplet/ccv
+OUTPUT_DIR=/net/projects/EduPo/data/KCV_combined
+RUN_FILE=run_temp.sh
+        
+INTRO="#!/bin/bash\n#SBATCH -J kveta\n#SBATCH -p cpu-troja\n\nsource ~/troja/virtualenv/edupo/bin/activate\n"
+
+echo -e $INTRO > $RUN_FILE
+
+counter=0
+for input_file in $INPUT_DIR/*.json
+do  
+    basename=$(basename ${input_file});
+    echo "python ccv2kveta.py $input_file $OUTPUT_DIR/$basename" >> $RUN_FILE;
+    counter=$((counter+1));
+    if [ $counter -eq 10000 ]; then
+        sbatch $RUN_FILE;
+        echo -e $INTRO > $RUN_FILE;
+        counter=0;
+    fi
+done
+sbatch $RUN_FILE
