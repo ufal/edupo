09458abae48be3120e07112982c885d266f5669e
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2024-11-14 15:06:05 +0100

    API testing done

--- webapp/rest_api_test.sh
+++ webapp/rest_api_test.sh
@@ -11,5 +11,10 @@ curl --data "poemid=78467" --header "Accept: text/plain" https://quest.ms.mff.cu
 echo
 echo
 
+curl --data "poemid=78467&accept=txt" https://quest.ms.mff.cuni.cz/edupo/genmotives
+
+echo
+echo
+
 curl -X POST --data "rhyme_scheme=ABBA" --data "metre=J" --header "Accept: application/json" https://quest.ms.mff.cuni.cz/edupo/gen \
     | python3 -m json.tool --no-ensure-ascii
