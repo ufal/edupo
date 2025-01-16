5bee0dff5c5228ebc8d2823412ade46a72b39fab
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-01-16 22:56:26 +0100

    bugfixes

--- backend/app.sh
+++ backend/app.sh
@@ -11,7 +11,7 @@ while true;
 do
     date
     echo Starting backend server...
-    EDUPO_SERVER_PATH=/edupo authbind --deep gunicorn app:app -b 0.0.0.0:5000 -w 2 --access-logfile=- --pid gunicorn.pid
+    EDUPO_SERVER_PATH=/edupo-api authbind --deep gunicorn app:app -b 0.0.0.0:5000 -w 2 --access-logfile=- --pid gunicorn.pid
     echo Backend server stopped, waiting $T for restart...
     sleep $T
     echo
