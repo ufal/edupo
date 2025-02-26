c7689c277efc82be9a8986d06d5fa1e907ac6772
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-02-26 15:04:03 +0100

    fix

--- watch_server.sh
+++ watch_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tail -f autodeploy.log frontend/logs/$(ls -t frontend/logs/|head -1) backend/logs/$(ls -t backend/logs/|head -1)
+tail -f autodeploy.log frontend/logs/$(ls -t frontend/logs/|head -1) backend/logs/$(ls -t backend/logs/|head -1) backend/logs/gen_tm.log backend/logs/gen_mc.log 
 
