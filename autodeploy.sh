a9e4da154c732020eab8d915309f28992c8ccfd1
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-03-27 20:51:30 +0100

    stop killing frontend in deploy

--- autodeploy.sh
+++ autodeploy.sh
@@ -6,7 +6,7 @@ then
 	git merge
     echo Killing backend server...
     kill -HUP $(cat backend/gunicorn.pid)
-    echo Killing frontend server...
-    kill -HUP $(cat frontend/gunicorn.pid)
+    #echo Killing frontend server...
+    #kill -HUP $(cat frontend/gunicorn.pid)
 fi >> autodeploy.log
 
