1c65a0d058bf7535b65d969fa4d5fc0f094be151
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-04-04 15:16:32 +0200

    fix

--- reload.sh
+++ reload.sh
@@ -2,8 +2,8 @@
 
 echo Reload generators...
 pgrep -e -a -f gen.py
-python3 gen.py mc 5010 2>> logs/gen_mc.log &
-python3 gen.py tm 5011 2>> logs/gen_tm.log &
+python3 gen.py mc 5010 2>> backend/logs/gen_mc.log &
+python3 gen.py tm 5011 2>> backend/logs/gen_tm.log &
 echo Reload backend server...
 kill -HUP $(cat backend/gunicorn.pid)
 
