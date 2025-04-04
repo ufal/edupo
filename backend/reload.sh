f5c717f6e2eae571500e65fa5b7081f58027df20
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-04-04 16:20:58 +0200

    outputjoin

--- backend/reload.sh
+++ backend/reload.sh
@@ -2,8 +2,8 @@
 
 echo Reload generators...
 pkill -e -f gen.py
-python3 gen.py mc 5010 2>> logs/gen_mc.log &
-python3 gen.py tm 5011 2>> logs/gen_tm.log &
+python3 gen.py mc 5010 &>> logs/gen_mc.log &
+python3 gen.py tm 5011 &>> logs/gen_tm.log &
 echo Reload backend server...
 kill -HUP $(cat gunicorn.pid)
 
