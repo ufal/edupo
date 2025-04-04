093519e377f2b262c59e90b7fe4fe3f564ec89e9
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-04-04 15:25:48 +0200

    pkill, not pgrep!

--- backend/reload.sh
+++ backend/reload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Reload generators...
-pgrep -e -a -f gen.py
+pkill -e -a -f gen.py
 python3 gen.py mc 5010 2>> logs/gen_mc.log &
 python3 gen.py tm 5011 2>> logs/gen_tm.log &
 echo Reload backend server...
