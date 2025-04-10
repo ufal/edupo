e1acaa50815d544c8e99bc8aefab52b0fb7d4480
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2025-04-10 13:16:29 +0200

    refactor local debug run of backend

--- backend/app_debug.sh
+++ backend/app_debug.sh
@@ -3,10 +3,8 @@
 # debug run
 # run only on local computer, not visible from outside
 echo Starting generator models...
-python3 gen.py zmq mc 5010 &
+python3 gen.py mc 5010 &
 echo mc process id $!
-python3 gen.py zmq tm 5011 &
-echo tm process id $!
 echo Starting Flask...
 flask run --debug
 
