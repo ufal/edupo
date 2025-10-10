5f514d4ce966917bce3bebe6df2769572b39e87d
David Mareček <marecek@ufal.mff.cuni.cz>
2025-10-10 12:48:52 +0200

    evaluateoru lze zadat datova sada a pocita i prumery

--- evaluation/evaluate_poems.py
+++ evaluation/evaluate_poems.py
@@ -7,23 +7,43 @@ sys.path.append("../kveta")
 from kveta import okvetuj
 from get_measures import get_measures
 
-print('filename', 'unknown_words', 'rhyming', 'metre_consistency', 'syllable_count_entropy', 'rhyming_consistency', 'chatgpt_meaning', 'chatgpt_syntax', 'chatgpt_rhyming', 'chatgpt_language', 'chatgpt_metrum', sep="\t")
+input_dir = sys.argv[1]
+output_file = sys.argv[2]
+measures = ('unknown_words', 'rhyming', 'metre_accuracy', 'chatgpt_meaning')
+avg=dict()
+total = 0
 
-for i in range(1, 1000):
-    num = str(i)
-    #if len(num) < 2:
-    #    num = '0'+num
-    filename = "poems/chatgpt-4o/poem_" + num + ".txt"
-    if not os.path.isfile(filename):
-        continue
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            results = get_measures(content)
-    except:
-        print('ERROR while processing', filename, file=sys.stderr)
+#print('filename', 'unknown_words', 'rhyming', 'metre_consistency', 'syllable_count_entropy', 'rhyming_consistency', 'chatgpt_meaning', 'chatgpt_syntax', 'chatgpt_rhyming', 'chatgpt_language', 'chatgpt_metrum', sep="\t")
+
+with open(output_file, "w") as f:
+    f.write('filename')
+    for m in measures:
+        f.write("\t"+m)
+        avg[m] = 0
+    f.write("\n")
+    for i in range(1, 1000):
+        num = str(i)
+        filename = input_dir+"/poem_" + num + ".txt"
+        if not os.path.isfile(filename):
+            continue
+        try:
+            with open(filename, 'r', encoding='utf-8') as file:
+                content = file.read()
+                results = get_measures(content)
+        except:
+            print('ERROR while processing', filename, file=sys.stderr)
         
-    if results:
-        print(filename, results['unknown_words'], results['rhyming'], results['metre_consistency'], results['syllable_count_entropy'], results['rhyming_consistency'], results['chatgpt_meaning'], results['chatgpt_syntax'], results['chatgpt_rhyming'], results['chatgpt_language'], results['chatgpt_metrum'], sep="\t", flush=True)
-    results = {}
+        if results:
+            print (f"poem_{num} processed", file=sys.stderr)
+            total += 1
+            f.write(filename)
+            for m in measures:
+                f.write("\t"+str(results[m]))
+                avg[m] += float(results[m])
+            f.write("\n")
+        results = {}
+
+for m in measures:
+    print("Average", m, ":", str(avg[m]/total))
+f.close()
 
