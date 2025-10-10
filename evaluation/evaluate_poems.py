e283173c6e560349f80d9974e5ffa2e20d23e4d2
David Mareček <marecek@ufal.mff.cuni.cz>
2025-10-10 11:00:31 +0200

    zapomenute komity z drivejska

--- evaluation/evaluate_poems.py
+++ evaluation/evaluate_poems.py
@@ -2,20 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 sys.path.append("../kveta")
 from kveta import okvetuj
 from get_measures import get_measures
 
 print('filename', 'unknown_words', 'rhyming', 'metre_consistency', 'syllable_count_entropy', 'rhyming_consistency', 'chatgpt_meaning', 'chatgpt_syntax', 'chatgpt_rhyming', 'chatgpt_language', 'chatgpt_metrum', sep="\t")
 
-for i in range(51, 101):
-    filename = "poems/poem_" + str(i) + ".txt"
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.read()
+for i in range(1, 1000):
+    num = str(i)
+    #if len(num) < 2:
+    #    num = '0'+num
+    filename = "poems/chatgpt-4o/poem_" + num + ".txt"
+    if not os.path.isfile(filename):
+        continue
     try:
-        results = get_measures(content)
+        with open(filename, 'r', encoding='utf-8') as file:
+            content = file.read()
+            results = get_measures(content)
     except:
         print('ERROR while processing', filename, file=sys.stderr)
-        raise
-    print(filename, results['unknown_words'], results['rhyming'], results['metre_consistency'], results['syllable_count_entropy'], results['rhyming_consistency'], results['chatgpt_meaning'], results['chatgpt_syntax'], results['chatgpt_rhyming'], results['chatgpt_language'], results['chatgpt_metrum'], sep="\t", flush=True)
+        
+    if results:
+        print(filename, results['unknown_words'], results['rhyming'], results['metre_consistency'], results['syllable_count_entropy'], results['rhyming_consistency'], results['chatgpt_meaning'], results['chatgpt_syntax'], results['chatgpt_rhyming'], results['chatgpt_language'], results['chatgpt_metrum'], sep="\t", flush=True)
+    results = {}
 
