46118ae7e8b1cc1ae878187c32f6af23c026e362
jdosoudil <jiri.antonin.dosoudil@gmail.com>
2025-10-25 20:49:14 +0200

    NEXT_PUBLIC_API_URL do env.production

--- frontend/src/components/layout/Footer/Footer.tsx
+++ frontend/src/components/layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Footer() {
     return (
         <footer className="mt-auto w-full flex flex-wrap justify-center items-center gap-6 px-docOffsetXSmall tablet:px-docOffsetXBig py-docOffsetY text-[14px] text-graySoft">
-            <Link className="underline" href={process.env.NEXT_PUBLIC_LINK_BASE + "credits"}>
+            <Link className="underline" href={(process.env.NEXT_PUBLIC_LINK_BASE || "/") + "credits"}>
                 O aplikaci
             </Link>
             {
