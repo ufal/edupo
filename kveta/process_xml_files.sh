d99f3e8d600b1379cbdadbea3907bf30a7db4de2
David Mareček <marecek@ufal.mff.cuni.cz>
2025-12-04 14:18:47 +0100

    okvetovani KSP od Karla

--- kveta/process_xml_files.sh
+++ kveta/process_xml_files.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-INPUT_DIR="/net/projects/EduPo/data/KCV_ROBERT_listopad_2025"
-OUTPUT_DIR="/net/projects/EduPo/data/KCV_ROBERT_okvetovano"
+#INPUT_DIR="/net/projects/EduPo/data/KCV_ROBERT_listopad_2025"
+#OUTPUT_DIR="/net/projects/EduPo/data/KCV_ROBERT_okvetovano"
+INPUT_DIR="/net/projects/EduPo/data/KSP_od_Karla/EduPo"
+OUTPUT_DIR="/net/projects/EduPo/data/KSP_od_Karla_okvetovano/EduPo"
+
+from_file="Bondy_Basnicke_spisy_I_2014_IV"
+kvetuj=true
 
 for file in "$INPUT_DIR"/*.xml; do
     echo "Processing $file..."
     basename=$(basename "$file" .xml)
-    python3 okvetuj_xml.py "$file" $OUTPUT_DIR/${basename// /_}
+    if [[ "$basename" == "$from_file" ]]; then
+        kvetuj=true
+    fi
+    if $kvetuj; then
+        python3 okvetuj_xml.py "$file" $OUTPUT_DIR/${basename// /_}
+    fi
 done
