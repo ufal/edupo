3aded611ae7366ee49a93f3d7c1640ad4a45a201
David Mareček <marecek@ufal.mff.cuni.cz>
2026-05-14 11:33:29 +0200

    cykli pres vsechny kombinace parametru

--- scripts/generate_ala_matej.py
+++ scripts/generate_ala_matej.py
@@ -2,8 +2,9 @@
 #coding: utf-8
 
 import sys
-
+import time
 import logging
+
 logging.basicConfig(
     format='%(asctime)s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
@@ -22,13 +23,63 @@ headers = {"accept": "application/json"}
 # response.encoding='utf8'
 # print(response.text)
 
+def generate_poem(data, filename):
+    
+    start_time = time.perf_counter()
+    logging.info(f"Generate with params: {data}")
+    response = requests.post(f"{base_url}/gen", data=data, headers=headers)
+    # response.encoding='utf8'
+    poemid = response.json()['id']
+    logging.info(f"Generated poem {poemid}")
+    # text = response.json()['plaintext']
+    # print('GENERATED', text, sep="\n")
+    end_time = time.perf_counter()
+
+    logging.info(f"Analyzing poem {poemid}")
+    response = requests.post(f"{base_url}/analyze", data={"poemid": poemid}, headers=headers)
+    j = response.json()
+
+    # add time to measures
+    j['measures']['time'] = end_time - start_time
+
+    #logging.info(f"Meaning: {j['measures']['chatgpt_meaning']}")
+    #logging.info(f"Unknown words: {j['measures']['unknown_words']}")
+    #logging.info(f"Rhyming: {j['measures']['rhyming']}")
+    #logging.info(f"Rhyming consistency: {j['measures']['rhyming_consistency']}")
+    # input parameters jsou v j['geninput']
+    
+    JAKEM = {
+        '': 'libovolném',
+        'old': 'starém',
+        'modern': 'novém',
+        'contemporary': 'současném',
+    }
+
+    RYM = {
+        'yes': 's rýmem',
+        'no': 'bez rýmu',
+        '': '(rýmování neurčeno)',
+    }
+   
+    with open(filename + '.json', 'w') as outfile:
+        json.dump(j, outfile, indent=4, ensure_ascii=False)
+    with open(filename + '.txt', 'w') as outfile:
+        jakem = JAKEM[data['old_style']]
+        rymovana = RYM[data['rhymed']]
+        zadani = f"Zadání: báseň v {jakem} stylu {rymovana}\n\n"
+        # ostatní parametry asi nemusíme vypisovat pro hodnocení?
+        print(zadani + j['plaintext'], file=outfile)
+        logging.info(f"Stored poem {poemid}.json and {poemid}.txt")
+   
+    return j['measures']
+
+
+
 
 data = {
         # MODEL
         'modelspec': 'gpt-4o-mini', 
         
-
-
         # MATEJOVY PARAMETRE
         
         # old / modern / contemporary / ''
@@ -49,55 +100,44 @@ data = {
         # yes / no / ''
         'rhymed': 'yes', 
 
-
-
         # PEVNÉ PARAMETRY, ASI NECHAT TAKTO
         'temperature': 0.7,
         'max_strophes': 2, 
         
         }
 
-
-logging.info(f"Generate with params: {data}")
-response = requests.post(f"{base_url}/gen", data=data, headers=headers)
-# response.encoding='utf8'
-poemid = response.json()['id']
-logging.info(f"Generated poem {poemid}")
-# text = response.json()['plaintext']
-# print('GENERATED', text, sep="\n")
-
-logging.info(f"Analyzing poem {poemid}")
-response = requests.post(f"{base_url}/analyze", data={"poemid": poemid}, headers=headers)
-j = response.json()
-logging.info(f"Meaning: {j['measures']['chatgpt_meaning']}")
-logging.info(f"Unknown words: {j['measures']['unknown_words']}")
-logging.info(f"Rhyming: {j['measures']['rhyming']}")
-logging.info(f"Rhyming consistency: {j['measures']['rhyming_consistency']}")
-# input parameters jsou v j['geninput']
+model = 'gpt-4o-mini'
+directory = '/net/projects/EduPo/data/DM_generovane'
 
 
-JAKEM = {
-    '': 'libovolném',
-    'old': 'starém',
-    'modern': 'novém',
-    'contemporary': 'současném',
-}
+poem_counter = 0
+avg_measures = {'chatgpt_meaning': 0, 'rhyming': 0, 'unknown_words': 0, 'time': 0}
 
-RYM = {
-    'yes': 's rýmem',
-    'no': 'bez rýmu',
-    '': '(rýmování neurčeno)',
-}
+for old_style in [ "old", "modern", "contemporary", "" ]:
+    for syllables_count in [ "short", "long", "" ]:
+        for poem_length in [ "short", "medium", "" ]:
+            for motives in [ ["láska"] , ["příroda"], ["město"], ["rodina"], ["čas"], [] ]:
+                for mood in [ "veselá", "smutná", "" ]:
+                    for rhymed in [ "yes", "no", "" ]:
+                        data = {
+                            'modelspec': model,
+                            'old_style': old_style,
+                            'syllables_count': syllables_count, 
+                            'poem_length': poem_length,
+                            'motives': motives,
+                            'mood': mood,
+                            'rhymed': rhymed, 
+                            'temperature': 0.7,
+                            'max_strophes': 2, 
+                            }
+                        poem_counter += 1
+                        filename = directory + '/' + model + '-' + str(poem_counter)
+                        measures = generate_poem(data, filename)
+                        for m in avg_measures.keys():
+                            avg_measures[m] += measures[m]
 
-with open(poemid + '.json', 'w') as outfile:
-    json.dump(j, outfile, indent=4, ensure_ascii=False)
-with open(poemid + '.txt', 'w') as outfile:
-    jakem = JAKEM[data['old_style']]
-    rymovana = RYM[data['rhymed']]
-    zadani = f"Zadání: báseň v {jakem} stylu {rymovana}\n\n"
-    # ostatní parametry asi nemusíme vypisovat pro hodnocení?
-    print(zadani + j['plaintext'], file=outfile)
-logging.info(f"Stored poem {poemid}.json and {poemid}.txt")
-
+for m in avg_measures.keys():
+    print(m, str(avg_measures[m] / poem_counter))
+                        
 
 
