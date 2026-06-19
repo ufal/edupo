f219b2aef113c4f0e243ecdd673f2e9c9b888f8c
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2026-06-19 12:02:21 +0200

    external path to API for edupo.cz

--- backend/app.sh
+++ backend/app.sh
@@ -18,7 +18,7 @@ do
     ./reload.sh
     echo Starting backend server...
     # TODO explicitly set MC_MODEL_PORT and TM_MODEL_PORT?
-    EDUPO_SERVER_PATH=/edupo-api authbind --deep gunicorn app:app -b 0.0.0.0:5000 -w 2 --access-logfile=- --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" [%({x-request-id}o)s]' --pid gunicorn.pid --timeout 180
+    EDUPO_SERVER_PATH=/api authbind --deep gunicorn app:app -b 0.0.0.0:5000 -w 2 --access-logfile=- --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" [%({x-request-id}o)s]' --pid gunicorn.pid --timeout 180
     echo Backend server stopped, waiting $T for restart...
     sleep $T
     echo
