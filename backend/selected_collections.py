7dfc61bcb8f0bdb915f5e703ba23300584206d46
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2026-08-24 16:39:03 +0200

    forgotten file

---
+++ backend/selected_collections.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+collectionsByAuthor = {
+    "Mácha, Karel Hynek": [
+      "Máj (in Básně a dramatické zlomky, in Spisy Karla Hynka Máchy, svazek 1)",
+    ],
+    "Erben, Karel Jaromír": [
+      "Kytice z básní",
+    ],
+    "Neruda, Jan": [
+      "Balady a romance",
+      "Písně kosmické",
+    ],
+    "Hálek, Vítězslav": [
+      "Večerní písně",
+    ],
+    "Březina, Otokar": [
+      "Tajemné dálky",
+      "Svítání na západě",
+      "Větry od pólů",
+      "Stavitelé chrámu",
+      "Ruce",
+    ],
+    "Karásek ze Lvovic, Jiří": [
+      "Zazděná okna",
+      "Kniha aristokratická",
+      "Sexus necans",
+      "Sodoma (in Spisy Jiřího Karáska ze Lvovic, svazek 5)",
+    ],
+    "Hlaváček, Karel": [
+      "Mstivá kantiléna",
+      "Pozdě k ránu",
+    ],
+    "Gellner, František": [
+      "Po nás ať přijde potopa!",
+      "Radosti života",
+    ],
+    "Wolker, Jiří": [
+      "Těžká hodina",
+      "Host do domu",
+    ],
+    "Nezval, Vítězslav": [
+      "Žena v množném čísle",
+      "Absolutní hrobař",
+      "Praha s prsty deště",
+    ],
+    "Seifert, Jaroslav": [
+      "Město v slzách",
+      "Samá láska",
+    ],
+    "Biebl, Konstantin": [
+      "Zlatými řetězy",
+      "S lodí, jež dováží čaj a kávu",
+    ],
+    "Holan, Vladimír": [
+      "Příběhy",
+      "Noc s Hamletem",
+    ],
+    "Bondy, Egon": [
+      "Básnické spisy I",
+    ],
+    "Reynek, Bohuslav": [
+      "Podzimní motýli",
+      "Podzimní motýli; Sníh na zápraží; Mráz v okně",
+      "Žízně",
+      "Rty a zuby",
+      "Odlet vlaštovek",
+      "Pieta",
+      "Smutek země",
+      "Setba samot",
+    ],
+    "Diviš, Ivan": [
+      "První hudba bratřím",
+      "Balada z regálu",
+    ],
+    "Krchovský, J. H.": [
+      "Já už chci domů",
+      "Poslední list",
+    ],
+    "Hruška, Petr": [
+      "Obývací nepokoje",
+      "Vždycky se ty dveře zavíraly",
+      "Auta vjíždějí do lodí",
+    ],
+    "Správcová, Božena": [
+      "Guláš z modrý krávy",
+      "Východ",
+      "Večeře",
+      "Samomluvy",
+      "Strašnice",
+      "Požární kniha",
+    ],
+    "Stehlíková, Olga": [
+      "Vykřičník jak stožár",
+      "0 čem mluví Matka, když mlčí",
+      "Týdny",
+    ],
+    "Rudčenková, Kateřina": [
+      "Ludwig",
+      "Není nutné, abyste mě navštěvoval",
+      "Chůze po dunách",
+    ],
+    "Malý, Radek": [
+      "Lunovis",
+      "Vraní zpěvy",
+      "Světloplaší",
+      "Malá tma",
+      "Větrní",
+      "Všehomír",
+      "Atlas bytostí",
+    ],
+    "Borkovec, Petr": [
+      "Polní práce",
+      "Prostírání do tichého",
+      "Mezi oknem, stolem a postelí",
+      "Poustevna, věštírna, loutkárna",
+      "Ochoz",
+    ],
+    "Iljašenko, Marie": [
+      "Osip míří na jih",
+      "Sv. Outdoor",
+    ],
+    "Těsnohlídek, Jan": [
+      "Rakovina",
+      "Hranice a zdi",
+      "Hlavně zachraň sebe",
+      "Násilí bez předsudků",
+      "Ještě je co ztratit",
+    ],
+    "Jirous, Ivan Martin": [
+      "Úloža",
+      "Okuje",
+      "Rattus norvegicus",
+      "Ubíječ labutí",
+      "Rok krysy",
+    ],
+    "Wernisch, Ivan": [
+      "Don Čičo má v klopě orchidej",
+      "Z dalekých žofínů",
+      "Růžovejch květů sladká vůně",
+      "Pekařova noční nůše",
+      "Na břehu",
+      "Ó kdežpak",
+      "Bez kufru se tak pěkně skáče po stromech neboli Nún",
+      "Lunojasno, když kolovrátky",
+      "Dřevěný dort",
+      "Zeleně bliká Hilversum",
+      "Ztroskotanec na břehu atlantském",
+      "Frc",
+      "S brokovnicí pod kabátem",
+      "Penthesilea",
+      "Nikam",
+      "Zlatomodrý konec stařičkého léta",
+      "Proslýchá se",
+      "Hlava na stole",
+    ],
+    "Fischerová, Viola": [
+      "Domek na vinici",
+      "Matečná samota",
+      "Odrostlá blízkost",
+      "Jak pápěří",
+      "Divoká dráha domovů",
+      "Babí hodina",
+      "Zádušní mše za Pavla Buksu",
+      "Nyní",
+      "Předkonec",
+      "Písečné dítě",
+    ],
+    "Hrabě, Václav": [
+      "Blues pro bláznivou holku",
+    ],
+}
